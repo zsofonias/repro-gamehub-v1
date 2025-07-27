735c71bdcfa33f3a7c69631f1a3a500369c460e8
Sofonias Girmay <sofonias.dev@gmail.com>
2025-07-27 23:58:57 +0300

    feat:add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+import { Provider as ChakraProvider } from './components/ui/provider.tsx';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
